bc64ed499334ea4fb5ff6b5f619cc910bcb5643c
Michael Ducharm <ducharmdev@gmail.com>
2022-08-20 21:29:24 -0400

    add helper ext

---
+++ Extensions/IEnumerableExtensions.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MarkovMelodyCreator.Extensions;
+
+public static class IEnumerableExtensions
+{
+    public static IEnumerable<(int, T)> WithIndex<T>(this IEnumerable<T> values)
+    {
+        var idx = 0;
+
+        foreach (var item in values)
+        {
+            yield return (idx, item);
+            idx++;
+        }
+
+    }
+}
